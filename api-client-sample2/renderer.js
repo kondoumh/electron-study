b4d9ac28cc6cd8ade9858c2694066ac119213d2f
kondoumh <kondoumh@gmail.com>
2019-02-20 13:15:45 +0900

    add api client example (tumblr)

---
+++ api-client-sample2/renderer.js
@@ -0,0 +1,27 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+onload = () => {
+    document.querySelector("#fetch").addEventListener("click", e => {
+        const blogName = document.querySelector("#blog_name").value;
+        const apiKey = document.querySelector("#api_key").value;
+        fetchPage(blogName, apiKey);
+    });
+}
+
+function fetchPage(blogName, apiKey) {
+    const url = `https://api.tumblr.com/v2/blog/${blogName}/info?api_key=${apiKey}`;
+    fetch(url).then(res => {
+        if (res.status === 200) {
+            res.json().then(data => {
+                console.log(data);
+                document.querySelector("#text").innerHTML += `${data.response.blog.description}`
+            })
+        } else {
+            console.log(res.status);
+        }
+    }).catch(err => {
+        console.log(err);
+    })
+}
