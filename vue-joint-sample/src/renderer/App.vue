1ccf409d9be7b2d32a2cd778219efe9328fed4aa
kondoumh <kondoumh@gmail.com>
2019-02-24 13:43:28 +0900

    add assets of vue-joint-dia

--- vue-joint-sample/src/renderer/App.vue
+++ vue-joint-sample/src/renderer/App.vue
@@ -1,15 +1,40 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <InputForm v-on:addNode="addNode" v-on:checkLink="checkLink" />
+    <JointDia v-bind:nodeName="nodeName" v-bind:linkMode="linkMode" />
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'vue-joint-sample'
+import JointDia from '@/components/JointDia'
+import InputForm from '@/components/InputForm'
+
+export default {
+  name: 'vue-joint-sample',
+  components: {
+    JointDia,
+    InputForm
+  },
+  data () {
+    return {
+      nodeName: '',
+      linkMode: false
+    }
+  },
+  methods: {
+    addNode (name) {
+      this.nodeName = name
+    },
+    checkLink (checkLink) {
+      this.linkMode = checkLink
+    }
   }
+}
 </script>
 
 <style>
-  /* CSS */
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 90%;
+}
 </style>
