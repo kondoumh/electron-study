20545ae813527d15dd96fb19c3dac9fc1ad6c84e
kondoumh <kondoumh@gmail.com>
2019-05-08 01:10:46 +0900

    trial data from api

--- vue-datatable-sample/renderer.js
+++ vue-datatable-sample/renderer.js
@@ -3,101 +3,30 @@
 // All of the Node.js APIs are available in this process.
 const app = new Vue({
   el: '#app',
-  data: {
+  async mounted () {
+    const rowsPerPage = this.pagination.rowsPerPage
+    const skip = (this.pagination.page - 1) * rowsPerPage
+    const res = await axios.get(`https://reqres.in/api/users?page=${skip}&per_page=${rowsPerPage}`)
+    this.items = res.data.data
+    //this.$store.commit('saveTableData', this.items)
+  },
+  methods: {
+    nzDate: function (dt) {
+      return moment(dt).format('DD/MM/YYYY')
+    }
+  },
+  data: () => ({
+    search: '',
+    // totalItems: 0,
+    items: [],
+    pagination: {
+      sortBy: 'Date'
+    },
     headers: [
-      {
-        text: 'Dessert (100g serving)',
-        align: 'left',
-        sortable: false,
-        value: 'name'
-      },
-      { text: 'Calories', value: 'calories' },
-      { text: 'Fat (g)', value: 'fat' },
-      { text: 'Carbs (g)', value: 'carbs' },
-      { text: 'Protein (g)', value: 'protein' },
-      { text: 'Iron (%)', value: 'iron' }
-    ],
-    desserts: [
-      {
-        name: 'Frozen Yogurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        iron: '1%'
-      },
-      {
-        name: 'Ice cream sandwich',
-        calories: 237,
-        fat: 9.0,
-        carbs: 37,
-        protein: 4.3,
-        iron: '1%'
-      },
-      {
-        name: 'Eclair',
-        calories: 262,
-        fat: 16.0,
-        carbs: 23,
-        protein: 6.0,
-        iron: '7%'
-      },
-      {
-        name: 'Cupcake',
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-        iron: '8%'
-      },
-      {
-        name: 'Gingerbread',
-        calories: 356,
-        fat: 16.0,
-        carbs: 49,
-        protein: 3.9,
-        iron: '16%'
-      },
-      {
-        name: 'Jelly bean',
-        calories: 375,
-        fat: 0.0,
-        carbs: 94,
-        protein: 0.0,
-        iron: '0%'
-      },
-      {
-        name: 'Lollipop',
-        calories: 392,
-        fat: 0.2,
-        carbs: 98,
-        protein: 0,
-        iron: '2%'
-      },
-      {
-        name: 'Honeycomb',
-        calories: 408,
-        fat: 3.2,
-        carbs: 87,
-        protein: 6.5,
-        iron: '45%'
-      },
-      {
-        name: 'Donut',
-        calories: 452,
-        fat: 25.0,
-        carbs: 51,
-        protein: 4.9,
-        iron: '22%'
-      },
-      {
-        name: 'KitKat',
-        calories: 518,
-        fat: 26.0,
-        carbs: 65,
-        protein: 7,
-        iron: '6%'
-      }
+      { text: 'ID', value: 'id' },
+      { text: 'First Name', value: 'first_name' },
+      { text: 'Last Name', value: 'last_name' },
+      { text: 'Avatar', value: 'avatar' }
     ]
-  }
+  })
 })
