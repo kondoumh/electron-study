9dccdb50c0edd1cc35876d9c9e8d416a40a9f785
kondoumh <kondoumh@gmail.com>
2019-05-08 12:42:45 +0900

    fetch scrapbox data

--- vue-datatable-sample/renderer.js
+++ vue-datatable-sample/renderer.js
@@ -6,8 +6,8 @@ const app = new Vue({
   async mounted () {
     const rowsPerPage = this.pagination.rowsPerPage
     const skip = (this.pagination.page - 1) * rowsPerPage
-    const res = await axios.get(`https://reqres.in/api/users?page=${skip}&per_page=${rowsPerPage}`)
-    this.items = res.data.data
+    const res = await axios.get(`https://scrapbox.io/api/pages/kondoumh`)
+    this.items = await res.data.pages
     //this.$store.commit('saveTableData', this.items)
   },
   methods: {
@@ -23,10 +23,11 @@ const app = new Vue({
       sortBy: 'Date'
     },
     headers: [
-      { text: 'ID', value: 'id' },
-      { text: 'First Name', value: 'first_name' },
-      { text: 'Last Name', value: 'last_name' },
-      { text: 'Avatar', value: 'avatar' }
+      { text: 'pin', value: 'pin' },
+      { text: 'views', value: 'views' },
+      { text: 'linked', value: 'linked' },
+      { text: 'updated', value: 'updated' },
+      { text: 'title', value: 'title' }
     ]
   })
 })
