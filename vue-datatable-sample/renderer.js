f1a604049a3319194ae189c3c30547f5e800223b
kondoumh <kondoumh@gmail.com>
2019-05-12 10:26:01 +0900

    apply server side pagination

--- vue-datatable-sample/renderer.js
+++ vue-datatable-sample/renderer.js
@@ -4,13 +4,17 @@
 const app = new Vue({
   el: '#app',
   async mounted () {
-    const rowsPerPage = this.pagination.rowsPerPage
-    const skip = (this.pagination.page - 1) * rowsPerPage
-    const res = await axios.get(`https://scrapbox.io/api/pages/kondoumh`)
-    this.items = await res.data.pages
+    this.fetchData()
     //this.$store.commit('saveTableData', this.items)
   },
   methods: {
+    async fetchData() {
+      const rowsPerPage = this.pagination.rowsPerPage
+      const skip = (this.pagination.page - 1) * rowsPerPage
+      const res = await axios.get(`https://scrapbox.io/api/pages/kondoumh?skip=${skip}&limit=25`)
+      this.items = await res.data.pages
+      this.totalPages = res.data.count
+    },
     formatDate: function(timestamp) {
       let date = new Date()
       date.setTime(timestamp * 1000)
@@ -22,20 +26,24 @@ const app = new Vue({
       return date.toLocaleString(navigator.language, options)
     }
   },
+  watch: {
+    pagination: {
+      handler() {
+        this.fetchData()
+      }
+    }
+  },
   data: () => ({
     search: '',
-    // totalItems: 0,
+    totalPages: 0,
     items: [],
-    pagination: {
-      descending: true,
-      sortBy: 'updated'
-    },
+    pagination: {},
     headers: [
-      { text: 'pin', value: 'pin', width: '5%' },
+      { text: 'pin', value: 'pin', sortable: false, width: '5%' },
       { text: 'views', value: 'views', width: '10%' },
       { text: 'linked', value: 'linked', width: '10%' },
       { text: 'updated', value: 'updated', width: '25%' },
-      { text: 'title', value: 'title', width: '30%'},
+      { text: 'title', value: 'title', sortable: false, width: '30%'},
       { text: 'image', value: 'image', sortable: false, width: '25%' }
     ]
   })
