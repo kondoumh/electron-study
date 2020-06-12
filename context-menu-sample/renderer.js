463ab5dc1a6f7cbeae12754dbf46a5c362b62faf
kondoumh <kondoumh@gmail.com>
2020-06-12 09:13:32 +0900

    Add context-menu-example

---
+++ context-menu-sample/renderer.js
@@ -0,0 +1,20 @@
+const {app, BrowserWindow} = require('electron');
+const contextMenu = require('electron-context-menu');
+
+contextMenu({
+	prepend: (defaultActions, params, browserWindow) => [
+		{
+			label: 'Rainbow',
+			// Only show it when right-clicking images
+			visible: params.mediaType === 'image'
+		},
+		{
+			label: 'Search Google for “{selection}”',
+			// Only show it when right-clicking text
+			visible: params.selectionText.trim().length > 0,
+			click: () => {
+				shell.openExternal(`https://google.com/search?q=${encodeURIComponent(params.selectionText)}`);
+			}
+		}
+	]
+});
