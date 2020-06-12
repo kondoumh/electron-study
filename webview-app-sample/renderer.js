f4b00c073254a482fd1974b86296eeb77a0e918d
kondoumh <kondoumh@gmail.com>
2020-06-12 16:57:22 +0900

    move to dom-ready event

--- webview-app-sample/renderer.js
+++ webview-app-sample/renderer.js
@@ -25,21 +25,24 @@ ipcRenderer.on("check", (sender, arg) => {
   console.log(arg);
 });
 
-contextMenu({
-  window: webview,
-  prepend: (defaultActions, params, browserWindow) => [
-    {
-      label: 'Rainbow',
-      // Only show it when right-clicking images
-      visible: params.mediaType === 'image'
-    },
-    {
-      label: 'Search Google for “{selection}”',
-      // Only show it when right-clicking text
-      visible: params.selectionText.trim().length > 0,
-      click: () => {
-        shell.openExternal(`https://google.com/search?q=${encodeURIComponent(params.selectionText)}`);
+webview.addEventListener("dom-ready", e => {
+  contextMenu({
+    window: webview,
+    prepend: (defaultActions, params, browserWindow) => [
+      {
+        label: 'Rainbow',
+        // Only show it when right-clicking images
+        visible: params.mediaType === 'image'
+      },
+      {
+        label: 'Search Google for “{selection}”',
+        // Only show it when right-clicking text
+        visible: params.selectionText.trim().length > 0,
+        click: () => {
+          shell.openExternal(`https://google.com/search?q=${encodeURIComponent(params.selectionText)}`);
+        }
       }
-    }
-  ]
+    ]
+  });
 });
+
