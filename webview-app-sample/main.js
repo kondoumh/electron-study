8ce551935fc5db56ddbce25cc9486702698b0b54
kondoumh <kondoumh@gmail.com>
2021-06-01 09:04:04 +0900

    update electron to v13

--- webview-app-sample/main.js
+++ webview-app-sample/main.js
@@ -1,63 +1,41 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow, Menu} = require('electron')
+const { app, BrowserWindow, Menu, webviewTag } = require('electron');
+const contextMenu = require('electron-context-menu');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
-function createWindow () {
-  // Create the browser window.
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
       webviewTag: true,
-      enableRemoteModule: true
+      contextIsolation: false
     }
-  })
+  });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
 
-  createMenu()
+  createMenu();
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
+    mainWindow = null;
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
 
 function createMenu() {
   const template = [
@@ -100,11 +78,11 @@ function createMenu() {
     template.unshift({
       label: "Debug",
       submenu: [
-        { role: "forceReload"},
+        { role: "forceReload" },
         { role: "toggledevtools" },
-        { 
+        {
           label: "open devTools for WebView",
-          click () {
+          click() {
             mainWindow.webContents.send("openDevTools");
           }
         }
@@ -114,3 +92,22 @@ function createMenu() {
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
 }
+
+contextMenu({
+  window: webviewTag,
+  prepend: (defaultActions, parameters, mainWindow) => [
+    {
+      label: 'Rainbow',
+      // Only show it when right-clicking images
+      visible: parameters.mediaType === 'image'
+    },
+    {
+      label: 'Search Google for “{selection}”',
+      // Only show it when right-clicking text
+      visible: parameters.selectionText.trim().length > 0,
+      click: () => {
+        shell.openExternal(`https://google.com/search?q=${encodeURIComponent(parameters.selectionText)}`);
+      }
+    }
+  ]
+});
--- webview-app-sample/renderer.js
+++ webview-app-sample/renderer.js
@@ -1,9 +1,5 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
 const { ipcRenderer } = require("electron");
 const webview = document.querySelector("#webview");
-const contextMenu = require('electron-context-menu');
 
 ipcRenderer.on("goBack", () => {
   if (webview.canGoBack()) {
@@ -24,25 +20,3 @@ ipcRenderer.on("openDevTools", () => {
 ipcRenderer.on("check", (sender, arg) => {
   console.log(arg);
 });
-
-webview.addEventListener("dom-ready", e => {
-  contextMenu({
-    window: webview,
-    prepend: (defaultActions, params, browserWindow) => [
-      {
-        label: 'Rainbow',
-        // Only show it when right-clicking images
-        visible: params.mediaType === 'image'
-      },
-      {
-        label: 'Search Google for “{selection}”',
-        // Only show it when right-clicking text
-        visible: params.selectionText.trim().length > 0,
-        click: () => {
-          shell.openExternal(`https://google.com/search?q=${encodeURIComponent(params.selectionText)}`);
-        }
-      }
-    ]
-  });
-});
-
