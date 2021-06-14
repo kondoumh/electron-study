675e55f7cd48050f182859921a5505e7ae929037
kondoumh <kondoumh@gmail.com>
2021-06-15 08:56:24 +0900

    add log

--- webview-app-sample/main.js
+++ webview-app-sample/main.js
@@ -94,6 +94,8 @@ function createMenu() {
 ipcMain.on("webview-ready", (e, url) => {
   const contents = webContents.getAllWebContents().filter(c => !c.getURL().startsWith('file://'));
   const content = contents.find(c => c.getURL() === url);
+  console.log(content.id);
+  console.log(content.getURL());
   contextMenu({
     window: content,
     prepend: (defaultActions, parameters, mainWindow) => [
