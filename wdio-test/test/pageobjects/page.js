17b6d0b0c27376a1cece6898306f589a1989c7a6
kondoumh <kondoumh@gmail.com>
2021-12-19 16:23:39 +0900

    format

--- wdio-test/test/pageobjects/page.js
+++ wdio-test/test/pageobjects/page.js
@@ -3,11 +3,11 @@
 * that is shared across all page objects
 */
 module.exports = class Page {
-    /**
-    * Opens a sub page of the page
-    * @param path path of the sub page (e.g. /path/to/page.html)
-    */
-    open(path) {
-        return browser.url(`https://the-internet.herokuapp.com/${path}`)
-    }
+  /**
+  * Opens a sub page of the page
+  * @param path path of the sub page (e.g. /path/to/page.html)
+  */
+  open(path) {
+    return browser.url(`https://the-internet.herokuapp.com/${path}`)
+  }
 }
