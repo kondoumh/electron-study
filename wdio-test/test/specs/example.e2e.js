5ea508b8d6579e8dbe72ffbc7bdeb8160a05f3e3
kondoumh <kondoumh@gmail.com>
2021-12-21 06:59:51 +0900

    capture screen

--- wdio-test/test/specs/example.e2e.js
+++ wdio-test/test/specs/example.e2e.js
@@ -3,11 +3,14 @@ const SecurePage = require('../pageobjects/secure.page');
 
 describe('My Login application', () => {
   it('should login with valid credentials', async () => {
+    await browser.saveScreenshot('screenshots/1.png');
     await LoginPage.open();
+    await browser.saveScreenshot('screenshots/2.png');
 
     await LoginPage.login('tomsmith', 'SuperSecretPassword!');
     await expect(SecurePage.flashAlert).toBeExisting();
     await expect(SecurePage.flashAlert).toHaveTextContaining(
       'You logged into a secure area!');
+    await browser.saveScreenshot('screenshots/3.png');
   });
 });
