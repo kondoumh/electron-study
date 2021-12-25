08292eec65c4de127e15d6691822942d7430b51c
kondoumh <kondoumh@gmail.com>
2021-12-25 15:44:31 +0900

    add tabbar as browserview

--- browserview-sample/main.js
+++ browserview-sample/main.js
@@ -7,15 +7,15 @@ function createWindow () {
     width: 800,
     height: 600
   });
-
+  setupTabBar(mainWindow);
   setupView(mainWindow, 'https://electronjs.org');
   setupView(mainWindow, 'https://www.google.co.jp');
   setupView(mainWindow, 'https://scrapbox.io/kondoumh');
 
   ['resize'].forEach(e => {
     mainWindow.on(e, () => {
-      mainWindow.getBrowserViews().forEach(view => {
-        resizeView(view);
+      mainWindow.getBrowserViews().forEach((view, index) => {
+        resizeView(view, index);
       })
     });
   });
@@ -26,13 +26,25 @@ function createWindow () {
 function setupView(win, url) {
   const view = new BrowserView();
   win.addBrowserView(view);
-  resizeView(view);
+  resizeView(view, 1);
   view.webContents.loadURL(url);
 }
 
-function resizeView(view) {
+function setupTabBar(win) {
+  const view = new BrowserView();
+  win.addBrowserView(view);
   const bound = mainWindow.getBounds();
-  view.setBounds({ x: 0, y: 30, width: bound.width, height: bound.height - 30 });
+  view.setBounds({ x: 0, y: 0, width: bound.width, height: 30 });
+  view.webContents.loadURL(`file://${__dirname}/tabbar.html`);
+}
+
+function resizeView(view, index) {
+  const bound = mainWindow.getBounds();
+  if (index === 0) {
+    view.setBounds({ x: 0, y: 0, width: bound.width, height: 30 });
+  } else {
+    view.setBounds({ x: 0, y: 30, width: bound.width, height: bound.height - 30 });
+  }
 }
 
 app.whenReady().then(() => {
@@ -55,19 +67,19 @@ function createMenu() {
         {
           label: "site1",
           click() {
-            mainWindow.setTopBrowserView(mainWindow.getBrowserViews()[0]);
+            mainWindow.setTopBrowserView(mainWindow.getBrowserViews()[1]);
           }
         },
         {
           label: "site2",
           click() {
-            mainWindow.setTopBrowserView(mainWindow.getBrowserViews()[1]);
+            mainWindow.setTopBrowserView(mainWindow.getBrowserViews()[2]);
           }
         },
         {
           label: "site3",
           click() {
-            mainWindow.setTopBrowserView(mainWindow.getBrowserViews()[2]);
+            mainWindow.setTopBrowserView(mainWindow.getBrowserViews()[3]);
           }
         },
         { role: "quit" }
