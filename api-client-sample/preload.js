907d42a5b04c965e10a9d3361bf7a3c0bda1c709
kondoumh <kondoumh@gmail.com>
2021-12-30 11:38:45 +0900

    apply contextBridge

---
+++ api-client-sample/preload.js
@@ -0,0 +1,23 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld(
+  'api', {
+    fetchPage: (projectName, pageName) => {
+      const url = `https://scrapbox.io/api/pages/${projectName}/${pageName}`;
+      fetch(url).then(res => {
+        if (res.status === 200) {
+          res.json().then(data => {
+            data.lines.forEach(element => {
+              document.querySelector("#text").innerHTML += `${element.text}<br>`;
+            });
+            document.querySelector("#image").src = data.image;
+          })
+        } else {
+          console.log(res.status);
+        }
+      }).catch(err => {
+        console.log(err);
+      });
+    }
+  }
+);
--- api-client-sample/renderer.js
+++ api-client-sample/renderer.js
@@ -2,24 +2,6 @@ onload = () => {
   document.querySelector("#fetch").addEventListener("click", e => {
     const projectName = document.querySelector("#project_name").value;
     const pageName = document.querySelector("#page_name").value;
-    fetchPage(projectName, pageName);
-  });
-}
-
-function fetchPage(projectName, pageName) {
-  const url = `https://scrapbox.io/api/pages/${projectName}/${pageName}`;
-  fetch(url).then(res => {
-    if (res.status === 200) {
-      res.json().then(data => {
-        data.lines.forEach(element => {
-          document.querySelector("#text").innerHTML += `${element.text}<br>`;
-        });
-        document.querySelector("#image").src = data.image;
-      })
-    } else {
-      console.log(res.status);
-    }
-  }).catch(err => {
-    console.log(err);
+    window.api.fetchPage(projectName, pageName);
   });
 }
