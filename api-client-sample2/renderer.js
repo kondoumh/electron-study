a1f7323af624a03ea1015a6ec67c4e66aaf9448e
kondoumh <kondoumh@gmail.com>
2021-12-30 12:05:59 +0900

    apply context brigde

--- api-client-sample2/renderer.js
+++ api-client-sample2/renderer.js
@@ -1,23 +1,7 @@
 onload = () => {
-  document.querySelector("#fetch").addEventListener("click", e => {
-    const blogName = document.querySelector("#blog_name").value;
-    const apiKey = document.querySelector("#api_key").value;
-    fetchPage(blogName, apiKey);
-  });
-}
-
-function fetchPage(blogName, apiKey) {
-  const url = `https://api.tumblr.com/v2/blog/${blogName}/info?api_key=${apiKey}`;
-  fetch(url).then(res => {
-    if (res.status === 200) {
-      res.json().then(data => {
-        console.log(data);
-        document.querySelector("#text").innerHTML += `${data.response.blog.description}`
-      })
-    } else {
-      console.log(res.status);
-    }
-  }).catch(err => {
-    console.log(err);
+  document.querySelector('#fetch').addEventListener('click', e => {
+    const blogName = document.querySelector('#blog_name').value;
+    const apiKey = document.querySelector('#api_key').value;
+    window.api.fetchPage(blogName, apiKey);
   });
 }
