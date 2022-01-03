39c2d3762eb18bbf3efd6229c5f27680e2edeac4
kondoumh <kondoumh@gmail.com>
2022-01-03 11:49:12 +0900

    add tabbar.html on the top

--- browserview-sample/main.js
+++ browserview-sample/main.js
@@ -7,10 +7,10 @@ function createWindow () {
     width: 800,
     height: 600
   });
-  setupTabBar(mainWindow);
   setupView(mainWindow, 'https://electronjs.org');
   setupView(mainWindow, 'https://www.google.co.jp');
   setupView(mainWindow, 'https://scrapbox.io/kondoumh');
+  mainWindow.loadFile('tabbar.html');
 
   ['resize'].forEach(e => {
     mainWindow.on(e, () => {
@@ -38,13 +38,9 @@ function setupTabBar(win) {
   view.webContents.loadURL(`file://${__dirname}/tabbar.html`);
 }
 
-function resizeView(view, index) {
+function resizeView(view) {
   const bound = mainWindow.getBounds();
-  if (index === 0) {
-    view.setBounds({ x: 0, y: 0, width: bound.width, height: 30 });
-  } else {
-    view.setBounds({ x: 0, y: 30, width: bound.width, height: bound.height - 30 });
-  }
+  view.setBounds({ x: 0, y: 30, width: bound.width, height: bound.height - 30 });
 }
 
 app.whenReady().then(() => {
@@ -67,19 +63,19 @@ function createMenu() {
         {
           label: "site1",
           click() {
-            mainWindow.setTopBrowserView(mainWindow.getBrowserViews()[1]);
+            mainWindow.setTopBrowserView(mainWindow.getBrowserViews()[0]);
           }
         },
         {
           label: "site2",
           click() {
-            mainWindow.setTopBrowserView(mainWindow.getBrowserViews()[2]);
+            mainWindow.setTopBrowserView(mainWindow.getBrowserViews()[1]);
           }
         },
         {
           label: "site3",
           click() {
-            mainWindow.setTopBrowserView(mainWindow.getBrowserViews()[3]);
+            mainWindow.setTopBrowserView(mainWindow.getBrowserViews()[2]);
           }
         },
         { role: "quit" }
