e3374af447d7e33e8590addaa11ad6015c02c409
kondoumh <kondoumh@gmail.com>
2022-01-05 09:09:47 +0900

    apply async await

--- browserview-sample/preload.js
+++ browserview-sample/preload.js
@@ -2,10 +2,10 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld(
   'api', {
-    tab1: () => ipcRenderer.invoke('tab1'),
-    tab2: () => ipcRenderer.invoke('tab2'),
-    switchPage: () => {
-      ipcRenderer.invoke('switch-to-electronjs');
+    tab1: async () => await ipcRenderer.invoke('tab1'),
+    tab2: async () => await ipcRenderer.invoke('tab2'),
+    switchPage: async () => {
+      await ipcRenderer.invoke('switch-to-electronjs');
       return 'fuga';
     }
   }
