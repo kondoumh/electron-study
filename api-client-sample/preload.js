b1613430a8d1ce3cafded2295d13459c146eab95
kondoumh <kondoumh@gmail.com>
2022-02-16 09:20:05 +0900

    refactor

--- api-client-sample/preload.js
+++ api-client-sample/preload.js
@@ -2,22 +2,17 @@ const { contextBridge } = require('electron');
 
 contextBridge.exposeInMainWorld(
   'api', {
-    fetchPage: (projectName, pageName) => {
+    fetchPage: async (projectName, pageName) => {
       const url = `https://scrapbox.io/api/pages/${projectName}/${pageName}`;
-      fetch(url).then(res => {
-        if (res.status === 200) {
-          res.json().then(data => {
-            data.lines.forEach(element => {
-              document.querySelector('#text').innerHTML += `${element.text}<br>`;
-            });
-            document.querySelector('#image').src = data.image;
-          })
-        } else {
-          console.log(res.status);
-        }
-      }).catch(err => {
-        console.log(err);
+      let data
+      const res = await fetch(url).catch(error => {
+        console.error(error);
+        return data;
       });
+      if (res.status === 200) {
+        data = await res.json();
+      }
+      return data;
     }
   }
 );
--- api-client-sample/renderer.js
+++ api-client-sample/renderer.js
@@ -1,7 +1,13 @@
 onload = () => {
-  document.querySelector("#fetch").addEventListener("click", e => {
+  document.querySelector("#fetch").addEventListener("click", async e => {
     const projectName = document.querySelector("#project_name").value;
     const pageName = document.querySelector("#page_name").value;
-    window.api.fetchPage(projectName, pageName);
+    const data = await window.api.fetchPage(projectName, pageName);
+    if (data) {
+      data.lines.forEach(element => {
+        document.querySelector('#text').innerHTML += `${element.text}<br>`;
+      });
+      document.querySelector('#image').src = data.image;
+    }
   });
 }
