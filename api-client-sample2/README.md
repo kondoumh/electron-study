4151e33efcc63a4967a27b4c06ece3315822352a
kondoumh <kondoumh@gmail.com>
2022-02-16 09:21:50 +0900

    remove

--- api-client-sample2/README.md
+++
@@ -1,3 +0,0 @@
-# Electron API client (Tumblr)
-
-A sample to get and display blog information using the Tumblr API.
--- api-client-sample2/renderer.js
+++
@@ -1,7 +0,0 @@
-onload = () => {
-  document.querySelector('#fetch').addEventListener('click', e => {
-    const blogName = document.querySelector('#blog_name').value;
-    const apiKey = document.querySelector('#api_key').value;
-    window.api.fetchPage(blogName, apiKey);
-  });
-}
